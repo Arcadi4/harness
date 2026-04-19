5bd5ca0c33c6c15c2602439ea1c29b30f5c0eb5c
4rcadia <97033226+Arcadi4@users.noreply.github.com>
2026-04-19 19:38:17 -0400

    style: Configurate prettier

---
+++ prettier.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "prettier"
+
+const config: Config = {
+  semi: false,
+  singleQuote: false,
+  trailingComma: "es5",
+  printWidth: 100,
+  tabWidth: 2,
+}
+
+export default config
